dd8a077733dbc32e68d38d65b9b43ca37aff6526
Jose Antonio Orellana Gomez <101654298+OteloxESP@users.noreply.github.com>
2023-05-09 15:29:31 +0200

    Avances mercado

--- app/src/main/java/com/example/oteloxtfgdam/activity/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/oteloxtfgdam/activity/ui/gallery/GalleryFragment.java
@@ -29,9 +29,16 @@ private FragmentGalleryBinding binding;
     binding = FragmentGalleryBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
         List<Item> itemList = new ArrayList<>();
-        itemList.add(new Item("Artículo 1", "02/01/2022", "$10.00"));
+        itemList.add(new Item("Artículo 1", "01/01/2022", "$10.00"));
         itemList.add(new Item("Artículo 2", "02/02/2022", "$20.00"));
         itemList.add(new Item("Artículo 3", "03/03/2022", "$30.00"));
+        itemList.add(new Item("Artículo 4", "04/04/2022", "$40.00"));
+        itemList.add(new Item("Artículo 5", "05/05/2022", "$50.00"));
+        itemList.add(new Item("Artículo 6", "06/06/2022", "$60.00"));
+        itemList.add(new Item("Artículo 7", "07/07/2022", "$70.00"));
+        itemList.add(new Item("Artículo 8", "08/08/2022", "$80.00"));
+        itemList.add(new Item("Artículo 9", "09/09/2022", "$90.00"));
+        itemList.add(new Item("Artículo 10", "10/10/2022", "$100.00"));
         LinearLayout linearLayout = root.findViewById(R.id.linear_layout);
 
         for (Item item : itemList) {
