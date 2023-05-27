827ed05a68769cbbc8bcfeafd8ebe8c0b996057b
Jose Antonio Orellana Gomez <101654298+OteloxESP@users.noreply.github.com>
2023-05-27 20:55:32 +0200

    Avances perfil

--- app/src/main/java/com/example/oteloxtfgdam/activity/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/example/oteloxtfgdam/activity/ui/perfil/PerfilFragment.java
@@ -1,9 +1,15 @@
 package com.example.oteloxtfgdam.activity.ui.perfil;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,22 +19,42 @@ import com.example.oteloxtfgdam.databinding.FragmentPerfilBinding;
 
 public class PerfilFragment extends Fragment {
 
-private FragmentPerfilBinding binding;
+    private static final int REQUEST_IMAGE_GALLERY = 1;
+    private FragmentPerfilBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        PerfilViewModel perfilViewModel =
-                new ViewModelProvider(this).get(PerfilViewModel.class);
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentPerfilBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    binding = FragmentPerfilBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding.profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seleccionarImagen();
+            }
+        });
 
-        /*final TextView textView = binding.textSlideshow;
-        perfilViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
         return root;
     }
 
-@Override
+
+
+
+    public void seleccionarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK && data != null) {
+            Uri imagenSeleccionada = data.getData();
+
+        }
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
